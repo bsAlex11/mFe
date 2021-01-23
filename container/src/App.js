8e33a5f93dad6d08bf519174d7af2655c9f9e8fb
Alex <baies.alexandru90@yahoo.ro>
2021-01-24 00:20:06 +0200

    Added css specific namespace

--- container/src/App.js
+++ container/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+});
 
 export default () => {
   return (
     <>
-      <h1>contaiiner</h1>
-      <MarketingApp/>
+      <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+          <Header/>
+          <h1>contaiiner</h1>
+          <MarketingApp/>
+        </BrowserRouter>
+      </StylesProvider>
     </>
   );
 };
